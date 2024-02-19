dc6112bd1bec0f4d710c81818760fc20f98db737
alexvolpe <alex.volpe010@gmail.com>
2024-02-19 17:31:05 -0500

    commit 2

---
+++ script.js
@@ -0,0 +1,13 @@
+const inputbox = document.getElementById("input-box");
+const listcontainer = document.getElementById("list-container");
+
+function addtask(){
+    if(inputbox.value === ''){
+    alert("you must write somethin!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputbox.value;
+        listcontainer.appendChild(li);
+    }
+}
